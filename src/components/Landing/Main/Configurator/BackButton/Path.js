30dc9b4413b97c399433121bc39311dcde760697
sofiacreate <sblas@create-store.com>
2022-08-02 11:02:25 +0200

    Revert "feature: first commit & scaffolding"
    This reverts commit 094476b95de99d01285bb6abb1068713dd117d44.

--- src/components/Landing/Main/Configurator/BackButton/Path.js
+++
@@ -1,7 +0,0 @@
-const Path = (prop) => {
-  return (<><path fillRule="evenodd" clipRule="evenodd" d={prop.d} fill="#3B3B3B" />
-  <path fillRule="evenodd" clipRule="evenodd" d={prop.e} fill="#3B3B3B" />
-  <path fillRule="evenodd" clipRule="evenodd" d={prop.f} fill="#3B3B3B" /></>);
-};
-
-export default Path;
