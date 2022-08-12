f0ecb3da17c788fd67c716eefe26e793deb222aa
sofiacreate <sblas@create-store.com>
2022-08-12 14:58:57 +0200

    add: commnts on imagefan of next steps to do

--- src/components/Landing/Main/Configurator/ImageFan.js
+++ src/components/Landing/Main/Configurator/ImageFan.js
@@ -4,15 +4,18 @@ const ImageFan = (props) => {
     const baseURL = "https://www.create-store.com/img/core/ikh/configurator/fans/images";
     //get the name's folder between url
     const folder = window.location.href.split("/serie/")[1].split("/")[0];
-
-    //get motor color
-
     //url personalizada
     const imgUrl = `${baseURL}/${folder}/${props.selectedMotor}/fan.jpg`;
-
     return <img className="c-configurator__image-picture-img" alt="fan" src={imgUrl} />;
   };
-
+//Voy a guardar cada paso en un estado y cada eleccion del usuario en otro estado
+//Empezaré seteando todo a nada
+// let serie = "",
+// motorColor = "",
+// bladesColor = "",
+// light = "";
+// el resultado final de la url tiene que ser algo como:
+// return `${baseURL}${serie}${motorColor}${bladesColor}${light}/fan.jpg`;
   return (
     <article className="c-configurator__image">
       <picture className="c-configurator__image-picture">{getImgUrl()}</picture>
