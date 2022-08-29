1506ca722ed6c907f9d60c1b65dcb202f7a8ea3b
sofiacreate <sblas@create-store.com>
2022-08-29 15:05:12 +0200

    wip: fix undefined values on getInfoBlades function

--- src/services/getColorBubbles.js
+++ src/services/getColorBubbles.js
@@ -1,21 +1,36 @@
-
 const getInfoMotor = (fanArr) => {
-    let availableMotor = [];
-    fanArr.map((eachColorMotor) => {
-      if (eachColorMotor.active !== false && !availableMotor.includes(eachColorMotor.motor)) {
-        availableMotor.push(eachColorMotor.motor);
-      }
-    });  
+  let availableMotor = [];
+  fanArr.map((eachColorMotor) => {
+    if (eachColorMotor.active !== false && !availableMotor.includes(eachColorMotor.motor)) {
+      availableMotor.push(eachColorMotor.motor);
+    }
+  });
 
-      return availableMotor;
-  };
+  return availableMotor;
+};
 //  colores de aspas
 const getInfoBlades = (fanArr, chosenMotor) => {
   let availableBlades = [];
+
   //filtro el array por medio del estado, que me devuelve el color seleccionado por el usuario del motor
-  const arrColorBlades = fanArr.filter((fan) => fan.motor === chosenMotor).map((fanBlades) => fanBlades.blades);
+  let newArr = [];
+  console.log("fanArr", fanArr);
+  fanArr.map((cadaFan) => {
+    if (cadaFan.active !== false) {
+      newArr.push(cadaFan.active);
+    }
+  });
+  console.log("newArr", newArr);
+  const arrColorBlades = fanArr
+    .filter((fan) => fan.motor === chosenMotor)
+    .map((fanBlades) => {
+      if (fanBlades.active !== false) {
+        return fanBlades.blades;
+      }
+    });
   availableBlades.push(arrColorBlades);
 
+  console.log("availableBlades", availableBlades);
   return availableBlades[0];
 };
 
