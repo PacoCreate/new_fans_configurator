6d002727d5e923a54e7f10dfe28e395c175d78fb
sofiacreate <sblas@create-store.com>
2022-08-30 12:18:13 +0200

    add: new comments about renderButtons

--- src/components/Landing/Main/Configurator/ButtonSelector/ButtonSelectorType.js
+++ src/components/Landing/Main/Configurator/ButtonSelector/ButtonSelectorType.js
@@ -3,7 +3,11 @@ const ButtonSelectorType = (props) => {
   function handleButton(ev) {
     //este condicional guarda el estado del motor elegido y aspas elegidas dependiendo del numero de paso en el que esté.
     if (props.numberStep === "2") {
-      //updateMaterial guarda en un estado el motor escogido para más tarde hacer el filtro con las apas. Estoy segura de que esto es prescindible con el estado donde guardo el motor. Solo lo renderizo en este paso porque en el tercer paso me petaría
+      //updateMaterial guarda en un estado el motor escogido para más tarde hacer el filtro con las apas. Estoy segura de que esto es prescindible con el estado donde guardo el motor, pero los parámetros no son mi fuerte. Creo que sería en:
+      // <ul className="c-configurator__selector-colors u-list-unstyled">
+      // {renderButtons(props.getInfoBubbles(fanArr, props.materialSelected, props.updateMotor, props.updateBlades))}<-----AQUI
+      // </ul>  
+      //Solo lo renderizo en este paso porque en el tercer paso me petaría
       props.updateMaterial(ev.target.getAttribute("data-color-id"));
       props.updateMotor(ev.target.getAttribute("data-color-id"));
     } else if (props.numberStep === "3") {
@@ -18,7 +22,6 @@ const ButtonSelectorType = (props) => {
       title={`${props.eachColorButton}`}
       onClick={handleButton}
     >
-      {" "}
     </li>
   );
 };
